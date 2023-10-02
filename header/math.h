ee2ce176966b875a9c03f349e82ed90f0752d861
Azmain Inquaid Haque <144453145+inquaid@users.noreply.github.com>
2023-10-02 15:54:49 +0600

    Create math.h

---
+++ header/math.h
@@ -0,0 +1,49 @@
+sqrt(x): Calculates the square root of a number x.
+
+pow(x, y): Computes x raised to the power of y.
+
+fabs(x): Returns the absolute (positive) value of a floating-point number x.
+
+ceil(x): Rounds a floating-point number x up to the nearest integer.
+
+floor(x): Rounds a floating-point number x down to the nearest integer.
+
+round(x): Rounds a floating-point number x to the nearest integer, with ties rounding to the nearest even number.
+
+trunc(x): Truncates a floating-point number x to an integer by removing its fractional part.
+
+fmod(x, y): Computes the remainder when x is divided by y.
+
+log(x): Computes the natural logarithm (base e) of a number x.
+
+log10(x): Computes the base-10 logarithm of a number x.
+
+exp(x): Computes the exponential function, e^x.
+
+sin(x): Computes the sine of an angle in radians.
+
+cos(x): Computes the cosine of an angle in radians.
+
+tan(x): Computes the tangent of an angle in radians.
+
+asin(x): Computes the arcsine (inverse sine) of a value, returning the angle in radians.
+
+acos(x): Computes the arccosine (inverse cosine) of a value, returning the angle in radians.
+
+atan(x): Computes the arctangent (inverse tangent) of a value, returning the angle in radians.
+
+atan2(y, x): Computes the arctangent of y/x, taking into account the signs of both x and y, and returning the angle in radians.
+
+sinh(x): Computes the hyperbolic sine of a value.
+
+cosh(x): Computes the hyperbolic cosine of a value.
+
+tanh(x): Computes the hyperbolic tangent of a value.
+
+isnan(x): Checks if a floating-point value is NaN (Not-a-Number).
+
+isinf(x): Checks if a floating-point value is positive or negative infinity.
+
+fmax(x, y): Returns the maximum of two floating-point values x and y.
+
+fmin(x, y): Returns the minimum of two floating-point values x and y.
