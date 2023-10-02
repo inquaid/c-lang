6144870b87152704d06b5fb2b85bddd311eaab06
Azmain Inquaid Haque <144453145+inquaid@users.noreply.github.com>
2023-10-02 13:58:16 +0600

    Create ctype.h

---
+++ header/ctype.h
@@ -0,0 +1,13 @@
+isalpha(int c): Checks if the given character c is an alphabetic character (either uppercase or lowercase letter).
+
+isdigit(int c): Checks if the given character c is a digit (0-9).
+
+isalnum(int c): Checks if the given character c is an alphanumeric character (either a letter or a digit).
+
+islower(int c): Checks if the given character c is a lowercase letter.
+
+isupper(int c): Checks if the given character c is an uppercase letter.
+
+tolower(int c): Converts the given uppercase character c to its corresponding lowercase character if it's an uppercase letter; otherwise, it returns the character unchanged.
+
+toupper(int c): Converts the given lowercase character c to its corresponding uppercase character if it's a lowercase letter; otherwise, it returns the character unchanged.
